663531cce6eab074db67081c66fefdc1a7c32d37
ziperlee <839728919@qq.com>
2023-05-12 22:46:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
       "pushplus_token": environ['PUSHPLUS_TOKEN'],
       "pushplus_topic": ""
   }
-  content = f"测试数据推送: environ['GP_TOKEN'],"
+  content = f"测试数据推送: {environ['GP_TOKEN']}"
   content_html = ""
   title = "测试数据推送"
   pushplus.push(config, content, content_html, title)
